5a639d6f9827c52839abdb325e72a9dc62e262d4
Surendar Vinayagamoorthy <svinayagamoorthy@dminc.com>
2020-01-22 00:14:25 +0530

    initial commit

---
+++ file_system.py
@@ -0,0 +1,24 @@
+from os import walk, listdir, path
+
+separator_file = '➥   '
+separator_folder = '⬇   '
+
+output_string = []
+output_string.append
+
+
+def traverseFolder(dir, level):
+    for file in listdir(dir):
+        abs_path = path.join(dir, file)
+        if (path.isdir(abs_path)):
+            output_string.append(separator_folder * level + file)
+            output_string.append(separator_folder * level + len(file) * '-')
+            traverseFolder(abs_path, level + 1)
+        else:
+            output_string.append(separator_folder * (level - 1) + separator_file + file)
+
+traverseFolder('D:\\DMI\\Learning\\', 0)
+
+with open('File_Structure.txt', 'wb') as file_structure:
+    file_structure.write('\n'.join(output_string).encode())
+    print('File written')
